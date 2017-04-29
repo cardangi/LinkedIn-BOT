df1afa6f73a179ea31fb3eb2a3f02a2ebb05293c
Carlos <car.dangi@hotmail.com>
2017-04-28 22:54:35 -0300

    py file

---
+++ BotdoLinkedin.py
@@ -0,0 +1,19 @@
+# Bot for getting friends in LinkedIn.
+# Coded by Carlos Daniel Giovanella
+# Macro made by me aswell
+# Requirements: Python 3+, iMacros, and Firefox.
+# Not tested on Linux/MacOS based systems
+
+import subprocess
+print (' -----------------------------Made by--------------------------------------')
+print ('  # Carlos Daniel Giovanella (car.dangi@hotmail.com)')
+print ('  # ')
+print ('  # Facebook: www.fb.com/KiritoKirigayaKazutoKunZ')
+print ('  # Github: github.com/cardangi')
+print ('  # LinkedIn: https://www.linkedin.com/in/carlos-d-870792128/')
+print ('  # ')
+print ('  # I do not take any responsibility and i are not liable for any damage caused.')
+print (' -------------------------------------------------------------------------')
+
+subprocess.call([r'C:\Program Files (x86)\Mozilla Firefox\firefox.exe', '-new-tab', 'imacros://run/?m=MacroLinkedin.iim'])
+
